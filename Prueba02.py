892b5092af8c45e63ff6f95c8ae615e97a4aabd5
fer-vasco <fernandovasconi@hotmail.com>
2024-10-05 19:36:43 -0300

    Update Prueba02.py

--- Prueba02.py
+++ Prueba02.py
@@ -1,8 +1,13 @@
 import streamlit as st
 from datetime import datetime
+import pandas as pd
 
 corrida = datetime.now()
 corrida = corrida.strftime('%m-%d-%y %H:%M')
 
 st.title('Resultados')
 st.write(f'Datos actualizados el {corrida}')
+
+d = {'col1': [1, 2], 'col2': [3, 4], 'col3': [3, 4], 'col4': [3, 4], 'col5': [3, 4]}
+df = pd.DataFrame(data=d)
+st.dataframe(df)
