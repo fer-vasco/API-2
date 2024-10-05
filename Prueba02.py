c84bce5087633716d971fdea43dfb5c256e7c486
fer-vasco <fernandovasconi@hotmail.com>
2024-10-05 19:32:37 -0300

    Update Prueba02.py

--- Prueba02.py
+++ Prueba02.py
@@ -2,6 +2,7 @@ import streamlit as st
 from datetime import datetime
 
 corrida = datetime.now()
+current_time = corrida.strftime('%m-%d-%y %H:%M')
 
 st.title('Resultados')
 st.write(f'Datos actualizados el {corrida}')
